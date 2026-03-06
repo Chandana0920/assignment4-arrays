1c65452d49d090a2f65a47ea4832e3b0c556cb4b
thejan <thejan.hdk@gmail.com>
2026-03-06 13:56:01 +0530

    Updated Assignment4.java

---
+++ Assignment4.java
@@ -0,0 +1,16 @@
+class Assignment4 {
+  
+  public static int[] removeConsecutiveDuplicates(int[] arr){
+  // Implement logic here
+  return null;
+}
+  
+public static void main(String[] args) {
+  int[] arr = {1,1,2,2,2,3,4,4,5};
+  int[] result = removeConsecutiveDuplicates(arr);
+  for(int i=0; i<result.length; i++){
+      System.out.println(result[i]);
+  }
+  
+  }
+}
